e5154e027baa754524a0a35cdc09aae966bebdda
jmeriaux <jmeriaux@gmail.com>
2015-06-06 15:57:20 -0700

    .

--- Plot3.R
+++ Plot3.R
@@ -6,16 +6,14 @@ if (!exists("hpconsumption") )
 #Reset device output
 dev.off();
 
-plot(hpcon$Tstamp,hpcon$Sub_metering_1, type = "n",xlab="",ylab="Energy sub metering");
-
 plot(hpcon$Tstamp,hpcon$Sub_metering_1, col="black", type = "l",xlab="",ylab="Energy sub metering");
 
 lines(hpcon$Tstamp,hpcon$Sub_metering_2, col="red",xlab="",ylab="Energy sub metering" );
 
 lines(hpcon$Tstamp,hpcon$Sub_metering_3,  col="blue",xlab="",ylab="Energy sub metering");
 
+legend("topright", pch="_", col = c("black", "red","blue"), lty=1, legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), pt.cex = 1);
 
-legend("topright", pch="____", col = c("black", "red","blue"), lty=1, legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), pt.cex = 0.4);
+dev.copy(device=pdf,file="./Rplot1.pdf");
 
-dev.copy(device=pdf,file="./Rplot3.pdf");
 dev.off();
